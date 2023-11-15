abbff014f74ecc589d885c02542a44cf66783548
Alanjcabrera <cabreraalan986@gmail.com>
2023-11-14 23:08:58 -0300

    reactProyect

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,142 +1,54 @@
 import { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
-import {Card,CardBody,Stack,CardFooter,Button,Heading,Text,Divider} from "@chakra-ui/react";
+import { Card, CardBody, Stack, CardFooter, Button, Heading, Text, Divider } from "@chakra-ui/react";
 import { CartContext } from "../Contex/ShoppingCartContex";
 import ItemCount from "./ItemCount";
 
 const ItemDetails = ({ producto }) => {
-  
-  const { id } = useParams();
-  
-  const { addItem } = useContext(CartContext);
-  
-  const [quantityAdded, setQuantityAdded] = useState(1)
-  /*
-  const filteredProduct = Array.isArray(producto)
-  ? producto.filter((p) => p.id === id)
-  : [];
 
-  const selectedProduct = filteredProduct[0];
-  
-  const handleOnAdd = () => {
-    if (!isNaN(quantityAdded) && quantityAdded > 0) {
-      const item = {
-        id, name: selectedProduct.name, price: selectedProduct.price}
-      addItem(item, quantityAdded)
-    } else {
-      console.log("Cantidad inválida")
-    }
-  };*/
+    const { id } = useParams();
 
-  return (
-    <>
-          <Card key={producto.id} direction={{ base: "column", sm: "row" }} overflow="hidden" variant="outline">
+    const { addItem } = useContext(CartContext);
+
+    const [quantityAdded, setQuantityAdded] = useState(1)
+
+    const filteredProduct = Array.isArray(producto)
+        ? producto.filter((p) => p.id === id)
+        : [];
+
+    const selectedProduct = filteredProduct[0];
+
+    const handleOnAdd = () => {
+        if (!isNaN(quantityAdded) && quantityAdded > 0) {
+            const item = {
+                id, name: producto.name, price: producto.price
+            }
+            addItem(item, quantityAdded)
+        } else {
+            console.log("Cantidad inválida")
+        }
+    };
+    return (
+        <Card key={producto.id} direction={{ base: "column", sm: "row" }} overflow="hidden" variant="outline">
             <Stack>
-              <CardBody>
-                <Heading size="md">{producto.name}</Heading>
-                <Divider />
-                <Text py="2">{producto.description}</Text>
-                <Divider />
-                <Text color="blue.600" fontSize="2xl">
-                  $ {producto.price}
-                </Text>
-              </CardBody>
-              <ItemCount quantity={quantityAdded} setQuantity={setQuantityAdded}/>
-              <CardFooter>
-                <Button variant="solid" colorScheme="blue" onClick={handleOnAdd}>
-                  Agregar al Carrito
-                </Button>
-              </CardFooter>
+                <CardBody>
+                    <Heading size="md">{producto.name}</Heading>
+                    <Divider />
+                    <Text py="2">{producto.description}</Text>
+                    <Divider />
+                    <Text color="blue.600" fontSize="2xl">
+                        $ {producto.price}
+                    </Text>
+                </CardBody>
+                <ItemCount quantity={quantityAdded} setQuantity={setQuantityAdded} />
+                <CardFooter>
+                    <Button variant="solid" colorScheme="blue" onClick={handleOnAdd}>
+                        Agregar al Carrito
+                    </Button>
+                </CardFooter>
             </Stack>
-          </Card>
-    </>
-  );
-};
+        </Card>
+    )
+}
 
 export default ItemDetails
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import { useContext, useState } from "react";
-import { useParams } from "react-router-dom";
-import {Card,CardBody,Stack,CardFooter,Button,Heading,Text,Divider} from "@chakra-ui/react";
-import { CartContext } from "../Contex/ShoppingCartContex";
-import ItemCount from "./ItemCount";
-
-const ItemDetails = ({ producto }) => {
-  
-  const { id } = useParams();
-  
-  const { addItem } = useContext(CartContext);
-  
-  const [quantityAdded, setQuantityAdded] = useState(1)
-  
-  const filteredProduct = Array.isArray(producto)
-  ? producto.filter((p) => p.id === id)
-  : [];
-
-  const selectedProduct = filteredProduct[0];
-  
-  const handleOnAdd = () => {
-    if (!isNaN(quantityAdded) && quantityAdded > 0) {
-      const item = {
-        id, name: selectedProduct.name, price: selectedProduct.price}
-      addItem(item, quantityAdded)
-    } else {
-      console.log("Cantidad inválida")
-    }
-  };
-
-  return (
-    <>
-      {filteredProduct.map((p) => {
-        return (
-          <Card key={p.id} direction={{ base: "column", sm: "row" }} overflow="hidden" variant="outline">
-            <Stack>
-              <CardBody>
-                <Heading size="md">{p.name}</Heading>
-                <Divider />
-                <Text py="2">{p.description}</Text>
-                <Divider />
-                <Text color="blue.600" fontSize="2xl">
-                  $ {p.price}
-                </Text>
-              </CardBody>
-              <ItemCount quantity={quantityAdded} setQuantity={setQuantityAdded}/>
-              <CardFooter>
-                <Button variant="solid" colorScheme="blue" onClick={handleOnAdd}>
-                  Agregar al Carrito
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-      );
-    })}
-    </>
-  );
-};
-
-export default ItemDetails*/
